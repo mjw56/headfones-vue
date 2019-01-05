814d2afef7a8cd0a0a7c07e5cfc0e4cbfafe01b5
Mike Wilcox <mwilcox56@gmail.com>
2019-01-05 15:10:14 -0500

    initial commit

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import { getCookie } from './helpers/cookies';
 
 Vue.use(Router);
 
@@ -12,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home,
+      beforeEnter(to, from, next) {
+        if (!!getCookie('jwt') === false) {
+          next('/login');
+        }
+      }
     },
     {
       path: '/about',
@@ -20,6 +26,14 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+    },
+    {
+      path: '/login',
+      name: 'login',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "login" */ './views/Login.vue'),
     },
   ],
 });
---
+++ src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="login">
+    <button v-on:click="login">Login</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  methods: {
+    login(event) {
+      window.location.replace('http://localhost:3004/auth/connect');
+    }
+  }
+})
+export default class Login extends Vue {}
+</script>
