31d9a40e8329aa9c39e750a4c5d2b431f2533df2
Mike Wilcox <mwilcox56@gmail.com>
2019-01-13 11:15:34 -0500

    mobile single column

--- src/components/SearchResultsCardBlock.vue
+++ src/components/SearchResultsCardBlock.vue
@@ -21,10 +21,14 @@
 .search-results-card-block__grid {
   display: grid;
   width: 100%;
-  grid-template-columns: repeat(2, 1fr);
+  grid-template-columns: repeat(1, 1fr);
   grid-column-gap: 20px;
   grid-row-gap: 20px;
   grid-auto-flow: row;
+
+  @media (min-width: 700px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
 
   @media (min-width: 1000px) {
     grid-template-columns: repeat(3, 1fr);
@@ -32,12 +36,6 @@
 
   @media (min-width: 1200px) {
     grid-template-columns: repeat(4, 1fr);
-  }
-
-  @media (max-width: 768px) {
-    grid-template-columns: repeat(2, 1fr);
-    grid-template-rows: repeat(3, 1fr);
-    grid-row-gap: 20px;
   }
 }
 </style>
